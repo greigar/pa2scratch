cfb495191b92f6189477f7465677bc92346496e5
greigar <greig.robertson@youdo.co.nz>
2017-04-05 15:59:05 +1200

    scratching with bars

--- plots.R
+++ plots.R
@@ -124,6 +124,7 @@ ggplot(yearly_baltimore_emissions_vehicle, aes(x = year, y = total_emission )) +
 ggsave("plot5.png")
 
 
+# --------------------------------------------------------------------------------
 
 # 6. Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle
 #    sources in Los Angeles County, California (𝚏𝚒𝚙𝚜 == "𝟶𝟼𝟶𝟹𝟽").
@@ -132,18 +133,18 @@ ggsave("plot5.png")
 
 # NEI <- readRDS("summarySCC_PM25.rds")
 # SCC <- readRDS("Source_Classification_Code.rds")
+#
 
 scc_vehicle <- SCC %>% filter( grepl("Mobile.*Vehicles.*", EI.Sector, ignore.case = TRUE) ) %>% select(SCC)
-
 emissions   <- NEI %>% filter(fips == "24510" | fips == "06037")
-
-#nei_scc_vehicle  <- emissions$SCC %in% scc_vehicle$SCC
-#nei_vehiclez     <- emissions[nei_scc_vehicle,]
 
 cities <- c("24510" = "Baltimore", "06037" = "LA")
 emissions$City <- cities[emissions$fips]
-
 nei_vehicle <- inner_join(emissions, scc_vehicle, by = "SCC" )
+
+#
+# LINE
+#
 
 yearly_emissions_vehicle <- nei_vehicle %>% group_by(year, City) %>% summarise(total_emissions = sum(Emissions))
 
@@ -157,15 +158,36 @@ ggplot(yearly_emissions_vehicle, aes(x = year, y = total_emissions) ) + facet_gr
 ggsave("plot6b.png")
 
 
-yearly_emissions_vehicle %>% filter(City == "Baltimore") -> x
+#
+# LAG
+#
 
-y <- x$total_emissions
+yearly_emissions_vehicle %>% filter(City == "Baltimore") -> baltimore
+yearly_emissions_vehicle %>% filter(City == "LA")        -> la
+
+y <- baltimore$total_emissions
 y - lag(y)
 
 y1 <- c( 342.9102,   327.3847 , -500.0939 )
 y2 <- c(  -212.511179 ,  -3.878441 , -42.154922 )
-y3 <- cbind(y1,y2)
 
 y3 <- as.data.frame( cbind(y1,y2) )
 
 ggplot(y3, aes(x = c(2002,2005,2008), y = y1, color = "red") ) + geom_line() + geom_line(aes(y = y2), color = "blue")
+
+# BOX -> too squished
+ggplot(bav, aes(x = year, y = Emissions)) + geom_boxplot(aes(group=year)) + ylim(0,5)
+
+# BAR
+nei_vehicle %>% filter(City == "Baltimore") -> bav
+nei_vehicle %>% filter(City == "LA") -> lav
+ggplot(bav, aes(x = year, y = Emissions)) + geom_bar( stat = "identity" ) + geom_line(aes(mean(Emmissions)))
+ggplot(lav, aes(x = year, y = Emissions)) + geom_bar( stat = "identity" )
+
+head( ggplot_build(g)$data[[1]] ) # get colors from ggplot, g is the ggplot
+
+ggplot(bav, aes(x = year, y = Emissions, fill = factor(year))) + geom_bar( stat = "identity" )  +
+  scale_fill_brewer(palette = "Set1") +
+  geom_hline(yintercept=bav_1999, color = "#E41A1C") # first colour from Set1
+
+
